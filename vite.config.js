57e3d56d0816daeb683685ce94e3e182090857c7
Vitaliy Abrosimov <vitalikabrosimov00@gmail.com>
2025-04-18 23:48:01 +0300

    двадцать третий коммит: создание pwa

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import path from 'path';
+import {VitePWA} from "vite-plugin-pwa";
 
 export default defineConfig({
     build: {
@@ -13,4 +14,21 @@ export default defineConfig({
         outDir: path.resolve(__dirname, 'dist'),
     },
     assetsInclude: ['**/*.woff2'],
+    plugins: [
+        VitePWA({
+            registerType: 'autoUpdate',
+            manifest: {
+                name: "Журнал мониторинга посещаемости",
+                short_name: "Журнал",
+                description: "Журнал для мониторинга посещаемости студентами аудиторных занятий",
+                icons: [
+                    {
+                        src: 'app-icon.png',
+                        sizes: '1280x1280',
+                        type: 'image/png'
+                    }
+                ]
+            }
+        })
+    ]
 });
